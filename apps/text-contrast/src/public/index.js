d590d1c3a8726ddd0f5945cb6f60283ef7638519
Gavin de Bruyn <me@gavindebruyn.com>
2023-02-09 15:53:04 +0200

    Create and display guess statistics
    Signed-off-by: Gavin de Bruyn <me@gavindebruyn.com>

--- apps/text-contrast/src/public/index.js
+++ apps/text-contrast/src/public/index.js
@@ -2,17 +2,28 @@ const testBlockElement = document.getElementById('test-block');
 const aiGuessBlockElement = document.getElementById('ai-guess-block');
 const darkButtonElement = document.getElementById('btn-dark');
 const lightButtonElement = document.getElementById('btn-light');
-const persistTrainingDataElement = document.getElementById('btn-persist-training-data');
-const clearTrainingDataElement = document.getElementById('btn-clear-training-data');
+const persistTrainingDataButtonElement = document.getElementById('btn-persist-training-data');
+const clearTrainingDataButtonElement = document.getElementById('past-five-guesses');
+const pastFiveGuessesElement = document.getElementById('past-five-guesses');
+const pastFifteenGuessesElement = document.getElementById('past-fifteen-guesses');
+const lastClearGuessesElement = document.getElementById('last-clear-guesses');
+
+let aiCurrentGuess = { darkProbability: null, lightProbability: null };
+let guessesSinceReload = [];
+let previousGuessCorrectPercentages = {
+    5: 0,
+    15: 0
+};
 
 registerEventHandlers();
 randomizeBackgroundColor();
+setButtonTextColors();
 
 function registerEventHandlers() {
     darkButtonElement.addEventListener('click', () => submitCorrectAnswer(true));
     lightButtonElement.addEventListener('click', () => submitCorrectAnswer(false));
-    persistTrainingDataElement.addEventListener('click', () => persistTrainingData());
-    clearTrainingDataElement.addEventListener('click', () => clearTrainingData());
+    persistTrainingDataButtonElement.addEventListener('click', () => persistTrainingData());
+    clearTrainingDataButtonElement.addEventListener('click', () => clearTrainingData());
 }
 
 function randomizeBackgroundColor() {
@@ -36,14 +47,17 @@ function getCurrentBackgroundColour() {
 
 function getAiPrediction() {
     const backgroundColour = getCurrentBackgroundColour();
+    aiGuessBlockElement.innerHTML = '<span>Loading Guess</span>';
+
     fetch(
         'http://localhost:3000/ai/colour-contrast/predict?colourRgbString=' + encodeURI(backgroundColour)
     )
     .then(result => result.json())
     .then(result => {
+        aiCurrentGuess = result;
         const hasDarkContrast = result.darkProbability > result.lightProbability;
-        console.log(result);
         const textColour = hasDarkContrast ? '#000000' : '#FFFFFF';
+        aiGuessBlockElement.innerHTML = `<span style="color: ${ textColour }">${ hasDarkContrast ? 'Dark Text' : 'Light Text' }</span>`
         aiGuessBlockElement.style.color = textColour;
     }).catch(error => console.error(error));
 }
@@ -60,7 +74,10 @@ function submitCorrectAnswer(hasDarkContrast) {
             body: JSON.stringify({ colourRgbString: encodeURI(backgroundColour), hasDarkContrast, hasLightContrast: !hasDarkContrast})
         }
     ).then(() => {
+        trackGuess();
+        setGuessStats();
         randomizeBackgroundColor();
+        if (guessesSinceReload.length % 5 === 0) setButtonTextColors();
     }).catch(error => console.error(error));
 }
 
@@ -90,4 +107,58 @@ function clearTrainingData() {
     ).then(() => {
         console.log('Training data has been cleared.');
     }).catch(error => console.error(error));
+}
+
+function trackGuess(isCorrectAnswerDarkContrast) {
+    guessesSinceReload.push({
+        isCorrect: isCorrectAnswerDarkContrast
+            ? aiCurrentGuess.darkProbability > aiCurrentGuess.lightProbability
+            : aiCurrentGuess.darkProbability < aiCurrentGuess.lightProbability,
+        guess: aiCurrentGuess
+    })
+}
+
+function setGuessStats() {
+    guessesSinceReload.length > 4
+        ? pastFiveGuessesElement.innerHTML = getPastGuessStatsWithin(5)
+        : pastFiveGuessesElement.display = 'none';
+    guessesSinceReload.length > 14
+        ? pastFifteenGuessesElement.innerHTML = getPastGuessStatsWithin(15)
+        : pastFifteenGuessesElement.display = 'none';
+
+    lastClearGuessesElement.innerHTML = getPastGuessStatsWithin(guessesSinceReload.length);
+}
+
+function getPastGuessStatsWithin(guesses) {
+    const guessesWithinRange = guessesSinceReload.slice(0, guesses);
+    const correctGuessesCount = guessesWithinRange.filter(x => x.isCorrect).length;
+    const correctGuessPercentage = Math.round(correctGuessesCount / guesses * 100);
+
+    const isTotalGuesses = guesses === guessesSinceReload.length;
+    const isImprovement = correctGuessPercentage > previousGuessCorrectPercentages[guesses];
+
+    previousGuessCorrectPercentages[guesses] = correctGuessPercentage;
+
+    return isTotalGuesses
+        ? `${correctGuessesCount} / ${guesses} (${correctGuessPercentage}%)`
+        : `<span style="color: ${isImprovement ? 'green' : 'red'}">${correctGuessesCount} / ${guesses} (${correctGuessPercentage}%) ${isImprovement ? '^' : 'v'}</span>`;
+}
+
+function setButtonTextColors() {
+    const buttons = [
+        lightButtonElement,
+        darkButtonElement,
+        persistTrainingDataButtonElement,
+        clearTrainingDataButtonElement
+    ]
+    buttons.forEach(button => {
+        fetch(
+            'http://localhost:3000/ai/colour-contrast/predict?colourRgbString=' + encodeURI(button.style.backgroundColor)
+        )
+            .then(result => result.json())
+            .then(result => {
+                const hasDarkContrast = result.darkProbability > result.lightProbability;
+                button.style.color = hasDarkContrast ? 'rgb(0, 0, 0)' : 'rgb(255, 255, 255)';
+            });
+    })
 }
